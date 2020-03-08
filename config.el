efab29060928f5a54f42a93e0060f190ed3f3bae
lloyar <lloyar@live.com>
2020-03-08 08:29:21 +0800

    [Add] add racket-mode

--- config.el
+++ config.el
@@ -2,6 +2,8 @@
 
 ;; Place your private configuration here! Remember, you do not need to run 'doom
 ;; sync' after modifying this file!
+;;(setq evil-default-state 'emacs)
+(setq initial-frame-alist (quote ((fullscreen . maximized))))
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets.
@@ -18,7 +20,7 @@
 ;;
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
-(setq doom-font (font-spec :family "monospace" :size 14))
+(setq doom-font (font-spec :family "monospace" :size 18))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
